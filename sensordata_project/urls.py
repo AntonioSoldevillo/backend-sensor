03a6e130094487adf3a93de41c731f36f3f54308
desireeshortiee <desireeganohay13@gmail.com>
2025-05-14 13:42:42 +0800

    first commit

---
+++ sensordata_project/urls.py
@@ -0,0 +1,15 @@
+# myproject/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from sensor.views import SensorDataViewSet
+
+router = DefaultRouter()
+router.register(r'sensor-data', SensorDataViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api/', include(router.urls)),
+]
