68d238d58d7ad500c8900fa0bc904983c3b75f26
desireeshortiee <desireeganohay13@gmail.com>
2025-05-14 20:25:45 +0800

    iosdhfoabdsob

--- sensordata_project/settings.py
+++ sensordata_project/settings.py
@@ -135,6 +135,7 @@ DJOSER = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173/",  # React Development Server
     "http://localhost:19006",  # Expo (React Native) Development Server
+    "http://112.198.99.107:8000"
 ]
 
 REST_FRAMEWORK = {
